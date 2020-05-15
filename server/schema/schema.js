60f31b7e26d44620b16825fb3cebebd87c5350ab
deanjheikkila <deanjheikkila@gmail.com>
2020-05-15 14:30:21 -0700

    added query for author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,41 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema, 
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+// dummy data
+const books = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+]
+
+const authors =  [
+    {name: 'Patrick Rothfuss', age: 44, id:"1"},
+    {name: 'Brandon Sanderson', age: 42, id:"2"},
+    {name: 'Terry Pratchett', age: 66, id:"3"},
+  ]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -16,10 +44,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
-            resolve(parent, args){
-                // get data from db / other source
-                // args.id
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                // get data from mongodb or otherwise
+                return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parents, args) {
+                return _.find(authors, { id: args.id });
             }
         }
     }
@@ -28,3 +63,9 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
     query: RootQuery
 })
+
+// Sample query
+// book(id: "2"){
+//     name
+//     genre
+// }
